08b6da1b53822410037b84632c59449567101dc5
Rakshit Arora <rakshit087@gmail.com>
2022-12-29 02:44:18 +0530

    saving form data in localstorage

--- pages/new.tsx
+++ pages/new.tsx
@@ -7,16 +7,25 @@ const { TextArea } = Input;
 
 const NewPage = () => {
   const [avatar, setAvatar] = useState<string | null>(null);
-  const [formData, setFormData] = useState<newCommunityInterface>({
-    name: '',
-    description: '',
-    pfp: '',
-    channels: [],
-  });
+  const [formData, setFormData] = useState<newCommunityInterface | null>(null);
   const [step, setStep] = useState(0);
 
   useEffect(() => {
-    setFormData({ ...formData, pfp: avatar });
+    const data = localStorage.getItem('formData');
+    if (data) {
+      setFormData(JSON.parse(data));
+    } else {
+      setFormData({
+        name: '',
+        description: '',
+        pfp: '',
+        channels: [],
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    formData && setFormData({ ...formData, pfp: avatar });
   }, [avatar]);
 
   return (
@@ -26,7 +35,16 @@ const NewPage = () => {
         background: 'linear-gradient(51.28deg, #CFFAD6 34.21%, #F3E7F9 68.88%)',
       }}
     >
-      {step === 0 && (
+      {/* make a loader */}
+
+      {!formData && (
+        <div className="flex flex-col items-center justify-center">
+          <div className="w-32 h-32 border-b-2 border-gray-900 rounded-full animate-spin"></div>
+          <p className="text-2xl font-semibold text-[#69248A] mt-4">Loading...</p>
+        </div>
+      )}
+
+      {step === 0 && formData && (
         <>
           <div className="mb-12 text-center">
             <p className="text-5xl font-semibold text-[#69248A] mb-1">Create Your Community</p>
@@ -42,19 +60,21 @@ const NewPage = () => {
               setFormData({ ...formData, name: e.target.value });
             }}
             className="h-12 mb-12 border-none w-96"
+            value={formData.name}
           />
           <Button
             type="primary"
             className="w-96 h-12 text-[24px] bg-gradient-to-r from-[#EF88D2] to-[#AF5CD6] border-none"
             onClick={() => {
               formData.name !== '' ? setStep(1) : alert('Please enter a name');
+              localStorage.setItem('formData', JSON.stringify(formData));
             }}
           >
             Next
           </Button>
         </>
       )}
-      {step === 1 && (
+      {step === 1 && formData && (
         <div className="flex flex-col w-[720px]">
           <div className="mb-12 text-center">
             <p className="text-5xl font-semibold text-[#69248A] mb-1">Introduce Your Community</p>
