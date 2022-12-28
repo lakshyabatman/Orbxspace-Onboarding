e783661a32f3432bf182d377abcb2ddac1604f2b
Rakshit Arora <rakshit087@gmail.com>
2022-12-29 02:22:16 +0530

    completed step 2

--- pages/new.tsx
+++ pages/new.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import { AvatarUpload } from '../components/AvatarUpload';
 import { newCommunityInterface } from '../types/newCommunity';
 
+const { TextArea } = Input;
+
 const NewPage = () => {
   const [avatar, setAvatar] = useState<string | null>(null);
   const [formData, setFormData] = useState<newCommunityInterface>({
@@ -51,6 +53,35 @@ const NewPage = () => {
             Next
           </Button>
         </>
+      )}
+      {step === 1 && (
+        <div className="flex flex-col w-[720px]">
+          <div className="mb-12 text-center">
+            <p className="text-5xl font-semibold text-[#69248A] mb-1">Introduce Your Community</p>
+            <p className="text-[24px] font-light">Welcome people to your community space </p>
+          </div>
+          <div className="mb-1 w-96">
+            <p className="text-left">Introduce your community</p>
+          </div>
+          <TextArea
+            placeholder="Eg. Crypto Club"
+            onChange={(e) => {
+              setFormData({ ...formData, description: e.target.value });
+            }}
+            autoSize={{ minRows: 12, maxRows: 6 }}
+            className="w-full mb-12"
+          />
+          <Button
+            type="primary"
+            className="w-96 h-12 text-[24px] bg-gradient-to-r from-[#EF88D2] to-[#AF5CD6] border-none mx-auto"
+            onClick={() => {
+              formData.description !== '' ? setStep(1) : alert('Please enter a description');
+              console.log(formData);
+            }}
+          >
+            Next
+          </Button>
+        </div>
       )}
     </div>
   );
