e75439d92cea9f21c40247546515b836e3ed4c46
Rakshit Arora <rakshit087@gmail.com>
2022-12-30 18:44:23 +0530

    adding loading to autg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import AppProvider from '../context/AppContext';
+import { AppContext } from '../context/AppContext';
+import { useContext } from 'react';
+import { Spin } from 'antd';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const context = useContext(AppContext);
+  const loading = context?.Loading;
+
   return (
     <AppProvider>
       <Component {...pageProps} />
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,4 +1,4 @@
-import { Button } from 'antd';
+import { Button, Spin } from 'antd';
 import { AppContext, NetworkType } from '../context/AppContext';
 import { useContext, useEffect } from 'react';
 import { useRouter } from 'next/router';
@@ -6,6 +6,7 @@ import { useRouter } from 'next/router';
 const AuthPage = () => {
   const context = useContext(AppContext);
   const router = useRouter();
+  const loading = context?.Loading;
 
   useEffect(() => {
     if (context?.currentUser) {
@@ -20,28 +21,38 @@ const AuthPage = () => {
         background: 'linear-gradient(51.28deg, #CFFAD6 34.21%, #F3E7F9 68.88%)',
       }}
     >
-      <div className="px-8 py-8 bg-white min-w-[720px]">
-        <div className="text-center">
-          <p className="text-5xl font-semibold text-[#69248A] mb-1">Connect to Orbis</p>
-          <p className="text-[24px] font-light mb-8">With your wallet</p>
-          <hr className="border-[#000000] mb-8" />
+      {loading ? (
+        <div className="flex items-center justify-center h-screen">
+          <Spin
+            tip="Loading"
+            size="large"
+          >
+            <div className="text-center" />
+          </Spin>
         </div>
-        <div className="flex flex-col items-center justify-center py-8">
-          <Button
-            onClick={() => context?.connectWallet(NetworkType.Ethereum)}
-            className="w-96 h-16 bg-[#B45008] text-white mb-4 text-3xl"
-          >
-            Metamask
-          </Button>
-          <Button
-            onClick={() => {
-              context?.connectWallet(NetworkType.Solana);
-            }}
-            className="w-96 h-16 bg-[#201191] text-white mb-4 text-3xl"
-          >
-            Phantom
-          </Button>
-          {/* <Button
+      ) : (
+        <div className="px-8 py-8 bg-white min-w-[720px]">
+          <div className="text-center">
+            <p className="text-5xl font-semibold text-[#69248A] mb-1">Connect to Orbis</p>
+            <p className="text-[24px] font-light mb-8">With your wallet</p>
+            <hr className="border-[#000000] mb-8" />
+          </div>
+          <div className="flex flex-col items-center justify-center py-8">
+            <Button
+              onClick={() => context?.connectWallet(NetworkType.Ethereum)}
+              className="w-96 h-16 bg-[#B45008] text-white mb-4 text-3xl"
+            >
+              Metamask
+            </Button>
+            <Button
+              onClick={() => {
+                context?.connectWallet(NetworkType.Solana);
+              }}
+              className="w-96 h-16 bg-[#201191] text-white mb-4 text-3xl"
+            >
+              Phantom
+            </Button>
+            {/* <Button
             onClick={() => {
               console.log('metamask');
             }}
@@ -49,8 +60,9 @@ const AuthPage = () => {
           >
             Wallet Connect
           </Button> */}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
